c1a58d56f550569d3b18298935f2707e8dc3c3be
vikesh <vikesh@hexwhale.com>
2017-03-26 14:39:24 +0530

    26/3/2017 2:40pm

---
+++ contact.php
@@ -0,0 +1,63 @@
+<section id="section4" class="section" data-anchor="page4">
+  <div class="container">
+    <div class="header">
+      Contact
+    </div>
+    <div class="content">
+      <div class="wrapper">
+        <div id="map"></div>
+
+      </div>
+
+       <div class="contact wrapper">
+
+         <form class="contact_form" action="index.html" method="post">
+           <div class="switch-btn">
+             View Contact Details
+           </div>
+           <div class="form-wrapper">
+             <div class="group">
+               <label for="name">Name</label>
+               <input type="text" name="name" value="">
+             </div>
+             <div class="group">
+               <label for="email">Email</label>
+               <input type="email" name="email" value="">
+             </div>
+             <div class="group">
+               <label for="phone">Phone</label>
+               <input type="phone" name="phone" value="">
+             </div>
+             <div class="group">
+               <label for="message">Message</label>
+               <textarea name="name" ></textarea>
+             </div>
+             <button type="submit" class="submit-btn" name="button">Send</button>
+           </div>
+
+         </form>
+         <div class="contact-details active">
+           <div class="switch-btn">
+             Leave Us a Message
+           </div>
+           <address class="address-wrap">
+             <div class="address">
+
+              Hexwhale Technologies Pvt Ltd,<br>
+              2nd Floor, RBH PEARL,<br>
+              No 28. 5th D main,<br>
+              HRBR 2nd Block. Kalyan Nagar <br>
+              Bangalore - 560043
+             </div>
+             <div class="phone">
+               <a href="call:9483870155">Phone: 9483870155</a>
+             </div>
+             <div class="email">
+               <a href="mailto:contact@processmonk.com">Email: contact@processmonk.com</a>
+             </div>
+           </address>
+         </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ partners.php
@@ -0,0 +1,10 @@
+<section id="section3" class="section" data-anchor="page3">
+  <div class="container">
+    <div class="header">
+      Partners
+    </div>
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+    </div>
+  </div>
+</section>
